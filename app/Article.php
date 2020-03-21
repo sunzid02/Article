e2f7eb202600611e55cae5add1624f2d72282f67
sunzid02 <sunzid02@gmail.com>
2020-03-21 20:53:25 +0600

    table name added in model

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $table = "articles";
+
+    protected $fillable = [
+        'user_id', 'title', 'body'
+    ];
 }
